ef847ec70db3c122d30979ef8f018cbee5ef6a5d
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-13 16:31:06 -0300

    :sparkles:(app): show pokemon list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Col, Input } from "antd";
+import PokemonList from "./components/PokemonList";
 
 function App() {
-  return <div className="App">Pokedux</div>;
+  return (
+    <div className="App">
+      <Col span={8} offset={8}>
+        <Input.Search placeholder="Buscar..." />
+      </Col>
+
+      <PokemonList />
+    </div>
+  );
 }
 
 export default App;
