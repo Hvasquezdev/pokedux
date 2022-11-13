bed39f87c1d3eb3631655229cbfdafabf617e8e4
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-13 16:34:47 -0300

    :sparkles:(pokemon card): add start outlined icon

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import Meta from "antd/lib/card/Meta";
 
@@ -10,6 +11,7 @@ function PokemonCard() {
       cover={
         <img src="https://assets.reedpopcdn.com/pokemon_go_ditto_disguises_A16ilOs.png/BROK/thumbnail/1200x900/quality/100/pokemon_go_ditto_disguises_A16ilOs.png" />
       }
+      extra={<StarOutlined />}
     >
       <Meta description="Normal" />
     </Card>
