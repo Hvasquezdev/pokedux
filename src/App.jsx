e31774926709a3773c4133f3d297172f34a1ab20
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-13 19:30:22 -0300

    :sparkles: add logo image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+
 import { Col, Input } from "antd";
 import PokemonList from "./components/PokemonList";
+import logo from "./assets/images/svg/logo.svg";
 
 function App() {
   return (
     <div className="App">
+      <Col span={4} offset={10} className="app-head">
+        <img src={logo} alt="Pokedux" />
+      </Col>
+
       <Col span={8} offset={8}>
         <Input.Search placeholder="Buscar..." />
       </Col>
