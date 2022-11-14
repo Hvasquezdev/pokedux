bf9303141d7e593e874daf4bb7a98e2ff10ab2b3
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-14 01:55:00 -0300

    :sparkles:(pokemon card): handle pokemon data from props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,11 +3,11 @@ import { StarOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import Meta from "antd/lib/card/Meta";
 
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
   return (
     <Card
       style={{ width: 250 }}
-      title="Ditto"
+      title={pokemon.name}
       cover={
         <img src="https://assets.reedpopcdn.com/pokemon_go_ditto_disguises_A16ilOs.png/BROK/thumbnail/1200x900/quality/100/pokemon_go_ditto_disguises_A16ilOs.png" />
       }
