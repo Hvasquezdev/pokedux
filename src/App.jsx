f6d6a1f48714dca2619348904ce0290846234f41
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-14 01:56:58 -0300

    :sparkles:(app): get pokemon list from service and set data to redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import React, { useEffect } from "react";
 import "./App.css";
 
 import { Col, Input } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { setPokemonList } from "./redux/actions";
 import PokemonList from "./components/PokemonList";
 import logo from "./assets/images/svg/logo.svg";
+import ApiPokemonService from "./core/services/Pokemon.api.service";
+
+const pokemonService = new ApiPokemonService();
 
 function App() {
+  const pokemonList = useSelector((state) => state.pokemon.list);
+  const dispatch = useDispatch();
+
+  const getPokemonList = async () => {
+    try {
+      const { results } = await pokemonService.getPokemonList({ limit: 151 });
+      dispatch(setPokemonList(results));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPokemonList();
+  }, []);
+
   return (
     <div className="App">
       <Col span={4} offset={10} className="app-head">
@@ -15,7 +37,7 @@ function App() {
         <Input.Search placeholder="Buscar..." />
       </Col>
 
-      <PokemonList />
+      <PokemonList list={pokemonList} />
     </div>
   );
 }
