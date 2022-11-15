2670acc400b0c779d3ecc190f519179419f8fd21
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-15 16:02:28 -0300

    :sparkleS: add thunk and custom middleware

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,7 @@
+import ApiPokemonService from "../core/services/Pokemon.api.service";
 import { SET_POKEMON_LIST } from "./actionTypes";
+
+const pokemonService = new ApiPokemonService();
 
 export const setPokemonList = (list) => ({
   type: SET_POKEMON_LIST,
@@ -6,3 +9,12 @@ export const setPokemonList = (list) => ({
     list,
   },
 });
+
+export const getPokemonList = () => async (dispatch) => {
+  try {
+    const pokemonList = await pokemonService.getPokemonList({ limit: 151 });
+    dispatch(setPokemonList(pokemonList));
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, compose, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
 import { logger } from "./middlewares";
 import reducers from "./reducers";
 
-const composeEnhancers = compose(applyMiddleware(logger));
+const composeEnhancers = compose(applyMiddleware(thunk, logger));
 
 export default legacy_createStore(reducers, composeEnhancers);
