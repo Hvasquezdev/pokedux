9050b3294790e74df75b8e2877349cd0ea16e5f8
Hector Vazquez <hvasquezdev@gmail.com>
2022-11-15 16:01:53 -0300

    :sparkles:(pokemon service): add pokemon parser and details methods

--- src/core/services/Pokemon.api.service.js
+++ src/core/services/Pokemon.api.service.js
@@ -5,13 +5,47 @@ class ApiPokemonService {
     this._baseUrl = "https://pokeapi.co/api/v2/";
   }
 
+  async getPokemonDetails(url) {
+    try {
+      const { data } = await axios.get(url);
+
+      return data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  getPokemonThumb(pokemon) {
+    const { sprites } = pokemon;
+    const altImage = sprites["front_default"] || "";
+    const image = sprites.other["official-artwork"]["front_default"] || "";
+
+    return image || altImage;
+  }
+
+  async getParsedPokemonList(baseList) {
+    const promises = baseList.map(async (pokemon) => {
+      const response = await this.getPokemonDetails(pokemon.url);
+
+      return {
+        ...response,
+        image: this.getPokemonThumb(response),
+        typesStr: response.types.map(({ type }) => type.name).join(", "),
+      };
+    });
+    return await Promise.all(promises);
+  }
+
   async getPokemonList(params) {
     try {
       const { data } = await axios.get(`${this._baseUrl}pokemon`, {
         params,
       });
+      const pokemonListWithDetails = await this.getParsedPokemonList(
+        data.results
+      );
 
-      return data;
+      return pokemonListWithDetails;
     } catch (error) {
       throw new Error(error);
     }
